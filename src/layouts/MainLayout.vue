9f7e67e11385ee292d3a5d327ef4f05d91093b1d
ArcticRug25 <wyw82839148@163.com>
2022-08-15 00:54:11 +0800

    feat: update step

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,13 +1,27 @@
 <script setup lang="ts">
-import Navbar from './components/Navbar.vue'
+import type { UseScrollReturn } from '@vueuse/core'
+import { useScroll } from '@vueuse/core'
+import { vScroll } from '@vueuse/components'
 import Footer from './components/Footer.vue'
+import Navbar from './components/Navbar.vue'
+
+function onScroll(state: UseScrollReturn) {
+  console.log(state) // {x, y, isScrolling, arrivedState, directions}
+}
+
+const { x, y, isScrolling, arrivedState, directions } = useScroll(document)
 </script>
 
 <template>
-  <div class="main-app">
-    <Navbar />
-    <slot></slot>
-    <Footer />
+  <div v-scroll="onScroll" class="h-[50000px]">
+    <n-layout>
+      <n-layout-header><Navbar /></n-layout-header>
+      <n-layout-content ref="mainAppRef">
+        {{ y }}
+        <slot></slot>
+      </n-layout-content>
+      <n-layout-footer><Footer /></n-layout-footer>
+    </n-layout>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,4 +4,8 @@
   <div>Home</div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+div {
+  @apply h-[125rem];
+}
+</style>
